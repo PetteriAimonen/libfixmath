cc1e97463ef6c5d5cef55191e203c60171f472b3
flatmush <flatmush@d3e1167c-abe1-51d5-8199-f9061ebe54e4>
2011-03-03 11:21:20 +0000

    Added int64.h which performs 64-bit calculations using 32-bit operations, this means that the FIXMATH_NO_64BIT macro can now be used to enable these operations on compilers without 64-bit support, note that some of these operations are slower than the built-in gcc ones (though some are faster too). sqrt can now be compiled without 64-bit compiler support. All remaining functions which aren't supported in 32-bit are now disabled when the FIXMATH_NO_64BIT macro is enable which should stop any compiler errors on compilers where 64-bit isn't supported.

--- libfixmath/fix16_sqrt.c
+++ libfixmath/fix16_sqrt.c
@@ -1,5 +1,5 @@
 #include "fix16.h"
-
+#include "int64.h"
 
 
 #ifndef FIXMATH_NO_CACHE
@@ -10,32 +10,36 @@ fix16_t _fix16_sqrt_cache_value[4096] = { 0 };
 
 
 fix16_t fix16_sqrt(fix16_t inValue) {
+	int neg = (inValue < 0);
+	if(neg)
+		inValue = -inValue;
+
 	#ifndef FIXMATH_NO_CACHE
 	fix16_t tempIndex = (((inValue >> 16) ^ (inValue >> 4)) & 0x00000FFF);
 	if(_fix16_sqrt_cache_index[tempIndex] == inValue)
-		return _fix16_sqrt_cache_value[tempIndex];
+		return (neg ? -_fix16_sqrt_cache_value[tempIndex] : _fix16_sqrt_cache_value[tempIndex]);
 	#endif
 
-	int64_t tempOp = inValue; tempOp <<= 16;
-	int64_t tempOut = 0;
-	int64_t tempOne = 0x4000000000000000ULL;
+	int64_t tempOp  = int64_const((inValue >> 16), (inValue << 16));
+	int64_t tempOut = int64_const(0, 0);
+	int64_t tempOne = int64_const(0x40000000UL, 0x00000000UL);
 
-	while(tempOne > tempOp)
-		tempOne >>= 2;
+	while(int64_cmp_gt(tempOne, tempOp))
+		tempOne = int64_shift(tempOne, -2);
 
-	while(tempOne != 0) {
-		if(tempOp >= tempOut + tempOne) {
-			tempOp -= tempOut + tempOne;
-			tempOut += tempOne << 1;
+	while(int64_cmp_ne(tempOne, int64_const(0, 0))) {
+		if(int64_cmp_ge(tempOp, int64_add(tempOut, tempOne))) {
+			tempOp  = int64_sub(tempOp, int64_add(tempOut, tempOne));
+			tempOut = int64_add(tempOut, int64_shift(tempOne, 1));
 		}
-		tempOut >>= 1;
-		tempOne >>= 2;
+		tempOut = int64_shift(tempOut, -1);
+		tempOne = int64_shift(tempOne, -2);
 	}
 
 	#ifndef FIXMATH_NO_CACHE
 	_fix16_sqrt_cache_index[tempIndex] = inValue;
-	_fix16_sqrt_cache_value[tempIndex] = tempOut;
+	_fix16_sqrt_cache_value[tempIndex] = int64_lo(tempOut);
 	#endif
 
-	return tempOut;
+	return (neg ? -int64_lo(tempOut) : int64_lo(tempOut));
 }
