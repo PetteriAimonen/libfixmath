e4bcd327a3338311cc62f9937b89ab7044976415
maruncz <marun1@email.cz>
2021-04-27 11:01:48 +0200

    added short mul test

--- tests/tests_basic.c
+++ tests/tests_basic.c
@@ -5,11 +5,11 @@ int test_abs_short(void)
 {
     for (unsigned i = 0; i < TESTCASES_COUNT; ++i)
     {
-        fix16_t a      = testcases[i];
-        double fa      = fix16_to_dbl(a);
-        fix16_t result = fix16_abs(a);
-        double fresult = fabs(fa);
-        double min     = fix16_to_dbl(fix16_minimum);
+        fix16_t a       = testcases[i];
+        double  fa      = fix16_to_dbl(a);
+        fix16_t result  = fix16_abs(a);
+        double  fresult = fabs(fa);
+        double  min     = fix16_to_dbl(fix16_minimum);
         if (fa <= min)
         {
 #ifndef FIXMATH_NO_OVERFLOW
@@ -28,11 +28,11 @@ int test_abs_long(void)
 {
     for (unsigned i = 0; i < 10000; ++i)
     {
-        fix16_t a      = -rand();
-        double fa      = fix16_to_dbl(a);
-        fix16_t result = fix16_abs(a);
-        double fresult = fabs(fa);
-        double min     = fix16_to_dbl(fix16_minimum);
+        fix16_t a       = -rand();
+        double  fa      = fix16_to_dbl(a);
+        fix16_t result  = fix16_abs(a);
+        double  fresult = fabs(fa);
+        double  min     = fix16_to_dbl(fix16_minimum);
         if (fa <= min)
         {
 #ifndef FIXMATH_NO_OVERFLOW
@@ -62,12 +62,14 @@ int test_add_short(void)
         {
             fix16_t a      = testcases[i];
             fix16_t b      = testcases[j];
+            fix16_t result = fix16_add(a, b);
+
             double fa      = fix16_to_dbl(a);
             double fb      = fix16_to_dbl(b);
-            fix16_t result = fix16_add(a, b);
             double fresult = fa + fb;
-            double max     = fix16_to_dbl(fix16_maximum);
-            double min     = fix16_to_dbl(fix16_minimum);
+
+            double max = fix16_to_dbl(fix16_maximum);
+            double min = fix16_to_dbl(fix16_minimum);
             if ((fa + fb > max) || (fa + fb < min))
             {
 #ifndef FIXMATH_NO_OVERFLOW
@@ -113,8 +115,42 @@ int test_mul_specific(void)
     return 0;
 }
 
+int test_mul_short()
+{
+    for (unsigned i = 0; i < TESTCASES_COUNT; ++i)
+    {
+        for (unsigned j = 0; j < TESTCASES_COUNT; ++j)
+        {
+            fix16_t a      = testcases[i];
+            fix16_t b      = testcases[j];
+            fix16_t result = fix16_mul(a, b);
+
+            double fa      = fix16_to_dbl(a);
+            double fb      = fix16_to_dbl(b);
+            double fresult = fa * fb;
+
+            double max = fix16_to_dbl(fix16_maximum);
+            double min = fix16_to_dbl(fix16_minimum);
+
+            if (fa * fb > max || fa * fb < min)
+            {
+#ifndef FIXMATH_NO_OVERFLOW
+                ASSERT_EQ_INT(result, fix16_overflow);
+#endif
+            }
+            else
+            {
+                ASSERT_NEAR_DOUBLE(fresult, fix16_to_dbl(result),
+                                   fix16_to_dbl(1));
+            }
+        }
+    }
+    return 0;
+}
+
 int test_mul(void)
 {
     TEST(test_mul_specific());
+    TEST(test_mul_short());
     return 0;
 }
